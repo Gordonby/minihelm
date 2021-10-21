8a8b29b96c75af955385a7d3dceebb2c48447416
Gordon Byers <gordon.byers@microsoft.com>
2021-10-21 21:48:48 +0100

    dir rename

--- samples/install-azurevote.ps1
+++ samples/install-azurevote.ps1
@@ -1,6 +1,6 @@
 #helm install externaldns ./externaldns --dry-run --debug --set externaldns.domainfilter="test"
 
 kubectl create ns az
-helm install azure-vote ./azurevote -n az
+helm install azure-vote ./azurevote-simple -n az
 kubectl get po -n az
 kubectl get svc -n az

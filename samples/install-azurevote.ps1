84d524940f1c7e2cd6bf27d287c1e19666b50159
Gordon Byers <gordon.byers@microsoft.com>
2021-10-22 08:17:07 +0100

    Update install-azurevote.ps1

--- samples/install-azurevote.ps1
+++ samples/install-azurevote.ps1
@@ -1,6 +1,5 @@
 #helm install externaldns ./externaldns --dry-run --debug --set externaldns.domainfilter="test"
 
-kubectl create ns az
-helm install azure-vote ./azurevote-simple -n az
+helm install azure-vote ./azurevote-simple -n az --create-namespace
 kubectl get po -n az
 kubectl get svc -n az

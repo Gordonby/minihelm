b02bde485aa3bad28ee6c130e820cdc96d5e3c5b
Gordonby <gordon.byers@microsoft.com>
2022-01-11 12:29:30 +0000

    new azurevote networkpolicy

--- samples/install-azurevote.ps1
+++ samples/install-azurevote.ps1
@@ -6,7 +6,7 @@ helm upgrade --install azurevotey ./azurevote --set $setparams -n default --dry-
 
 helm upgrade --install azurevotey ./azurevote --set $setparams -n default
 
-helm upgrade --install azure-vote-public https://github.com/Gordonby/minihelm/blob/main/samples/AzureVote-0.6.0.tgz?raw=true --set $setparams -n default
+helm upgrade --install azure-vote-public https://github.com/Gordonby/minihelm/blob/main/samples/AzureVote-0.7.0.tgz?raw=true --set $setparams -n default
 
 
 kubectl get po

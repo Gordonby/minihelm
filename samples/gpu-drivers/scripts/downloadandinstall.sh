ee24cc187121451ba50d335a30bbb88f459b64a8
Gordonby <gordon.byers@microsoft.com>
2023-01-05 12:25:38 +0000

    fail on 404
    Signed-off-by: Gordonby <gordon.byers@microsoft.com>

--- samples/gpu-drivers/scripts/downloadandinstall.sh
+++ samples/gpu-drivers/scripts/downloadandinstall.sh
@@ -7,29 +7,33 @@ set -uo pipefail
 
 set -x
 
+echo "Script for NVIDIA driver install $GPU_DV"
+
 open_devices="$(lsof /dev/nvidia* 2>/dev/null)"
 nvidia_modprobe_active="$(systemctl is-active nvidia-modprobe)"
 nvidia_device_plugin_active="$(systemctl is-active nvidia-device-plugin)"
 
 echo "Open devices: $open_devices"
-echo "nvidia-modprobe active: $nvidia_modprobe_active"
-echo "nvidia-device-plugin active: $nvidia_device_plugin_active"
+echo "nvidia-modprobe active: $nvidia_modprobe_active" #The nvidia-modprobe utility is used by user-space NVIDIA driver components to make sure the NVIDIA kernel module is loaded
+echo "nvidia-device-plugin active: $nvidia_device_plugin_active" #The NVIDIA device plugin for Kubernetes
 
 if [ -n "$open_devices" ]; then
     if [ "$nvidia_modprobe_active" == "active" ]; then
+    echo "Stopping nvidia-modprobe"
     systemctl stop nvidia-modprobe
     fi
     if [ "$nvidia_device_plugin_active" == "active" ]; then
+    echo "Stopping nvidia-device-plugin"
     systemctl stop nvidia-device-plugin
     fi
 fi
 
-echo "Installing NVIDIA driver $GPU_DV"
+
 GPU_DEST=/usr/local/nvidia
 log_file_name="/var/log/nvidia-installer-$(date +%s).log"
 KERNEL_NAME=$(uname -r)
 
-#check for and uninstall existing driver version
+#check for existing driver version
 if [ -f "${GPU_DEST}/bin/nvidia-smi" ]; then
     echo "found existing nvidia-smi, checking version..."
     existing_version="$(nvidia-smi | grep "Driver Version" | cut -d' ' -f3)"
@@ -37,6 +41,20 @@ if [ -f "${GPU_DEST}/bin/nvidia-smi" ]; then
     echo "desired version $GPU_DV matches existing version $existing_version, exiting early"
     exit 0
     fi
+fi
+
+#Download the new driver
+if [ ! -d "${GPU_DEST}" ]; then
+    mkdir -p ${GPU_DEST}
+fi
+
+set -e
+
+echo "downloading driver version $GPU_DV from nvidia website"
+curl -fLS https://us.download.nvidia.com/tesla/$GPU_DV/NVIDIA-Linux-x86_64-${GPU_DV}.run -o ${GPU_DEST}/nvidia-drivers-${GPU_DV} --fail
+
+#uninstall existing driver
+if [ -f "${GPU_DEST}/bin/nvidia-smi" ]; then
     if [ ! -z "$existing_version" ]; then
     echo "uninstalling driver version $existing_version to install version $GPU_DV"
     # nvidia uninstall requires kubelet to stop, throw a trap here as well as below
@@ -52,16 +70,6 @@ if [ -f "${GPU_DEST}/bin/nvidia-smi" ]; then
     echo "reboot/restarting kubelet fixes it."
     fi
 fi
-
-#Download the driver
-if [ ! -d "${GPU_DEST}" ]; then
-    mkdir -p ${GPU_DEST}
-fi
-
-set -e
-
-curl -fLS https://us.download.nvidia.com/tesla/$GPU_DV/NVIDIA-Linux-x86_64-${GPU_DV}.run -o ${GPU_DEST}/nvidia-drivers-${GPU_DV}
-
 
 # !!DANGER!! but necessary because kubelet holds /dev/nvidia* open
 # can't reinstall drivers without forcing that closed

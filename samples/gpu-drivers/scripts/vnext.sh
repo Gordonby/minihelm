3f0fd7dadcd43cdecdd866ad2d284af7ae6f8011
Gordonby <gordon.byers@microsoft.com>
2023-02-20 10:30:43 +0000

    intro vnext script
    Signed-off-by: Gordonby <gordon.byers@microsoft.com>

--- samples/gpu-drivers/scripts/vnext.sh
+++ samples/gpu-drivers/scripts/vnext.sh
@@ -18,8 +18,8 @@ if [ -f "${GPU_DEST}/bin/nvidia-smi" ]; then
     echo "found existing nvidia-smi, checking version..."
     existing_version="$(nvidia-smi | grep "Driver Version" | cut -d' ' -f3)"
     if [ "$existing_version" == "$GPU_DV" ]; then
-    echo "desired version $GPU_DV matches existing version $existing_version, exiting early"
-    exit 0
+        echo "desired version $GPU_DV matches existing version $existing_version, exiting early"
+        exit 0
     fi
 fi
 
@@ -33,7 +33,9 @@ set -e
 echo "downloading driver version $GPU_DV from nvidia website"
 curl -fLS https://us.download.nvidia.com/tesla/$GPU_DV/NVIDIA-Linux-x86_64-${GPU_DV}.run -o ${GPU_DEST}/nvidia-drivers-${GPU_DV} --fail
 
+set +e
 
+echo "Checking and closing any processes that might be using the GPU"
 open_devices="$(lsof /dev/nvidia* 2>/dev/null)"
 nvidia_modprobe_active="$(systemctl is-active nvidia-modprobe)"
 nvidia_device_plugin_active="$(systemctl is-active nvidia-device-plugin)"
@@ -53,6 +55,14 @@ if [ -n "$open_devices" ]; then
     fi
 fi
 
+echo "setting up 'finally' trap to restart kubelet"
+trap 'systemctl restart kubelet' EXIT SIGINT SIGTERM
+
+echo "stopping kubelet"
+# Necessary because kubelet holds /dev/nvidia* open
+# can't reinstall drivers without forcing that closed
+systemctl stop kubelet
+
 echo "Searching for nvidia device plugin processes"
 PROCMATCHES=$(ps -aux | grep nvidia-device-plugin | grep -v grep -c)
 
@@ -66,15 +76,11 @@ else
     echo "Process not found"
 fi
 
+#set -e
+
 #uninstall existing driver
 if [ -f "${GPU_DEST}/bin/nvidia-smi" ]; then
     if [ ! -z "$existing_version" ]; then
-        echo "uninstalling driver version $existing_version to install version $GPU_DV"
-        # nvidia uninstall requires kubelet to stop, throw a trap here as well as below
-        # this covers existing driver case, outside the conditionals cover new drivers
-
-        trap 'systemctl restart kubelet' EXIT SIGINT SIGTERM
-        systemctl stop kubelet
         ${GPU_DEST}/bin/nvidia-uninstall --silent
     fi
     if [ -z "$existing_version" ]; then
@@ -84,11 +90,8 @@ if [ -f "${GPU_DEST}/bin/nvidia-smi" ]; then
     fi
 fi
 
-# !!DANGER!! but necessary because kubelet holds /dev/nvidia* open
-# can't reinstall drivers without forcing that closed
-trap 'systemctl restart kubelet' EXIT SIGINT SIGTERM
 
-systemctl stop kubelet
+ps -aux | grep nvidia-device-plugin | grep -v grep -c
 
 echo "running installer"
 sh $GPU_DEST/nvidia-drivers-$GPU_DV -s -k=$KERNEL_NAME --log-file-name=${log_file_name} -a --no-drm --dkms --utility-prefix="${GPU_DEST}" --opengl-prefix="${GPU_DEST}" 2>&1
